8d5fb9b16d49f61ce2198c50dd1d2132f18a4ae4
Andrei Panferov <andrei@panferov.org>
2026-01-14 14:25:52 +0100

    nvidia norequat x patch

--- src/models/quantization/schemes/nvidia.py
+++ src/models/quantization/schemes/nvidia.py
@@ -30,16 +30,16 @@ class Nvidia_fn(torch.autograd.Function):
             input_fp4 = rtn_16x16s_fp4_kernel_wrapper(input, scale_override=1.0, group_size=Nvidia_fn.group_size)
             weight_fp4 = rtn_16x16s_fp4_kernel_wrapper(weight, scale_override=1.0, group_size=Nvidia_fn.group_size)
 
-        ctx.save_for_backward(input_fp4, weight_fp4)
+        ctx.save_for_backward(input, weight_fp4)
         return F.linear(input_fp4, weight_fp4)
 
     @torch.compile(dynamic=False)
     @staticmethod
     def backward(ctx, grad_output):
         # Load ctx and reshape
-        input_fp4, weight_fp4 = ctx.saved_tensors
+        input, weight_fp4 = ctx.saved_tensors
         
-        input_fp4 = input_fp4.reshape(ctx.batch * ctx.seq, ctx.in_dim)
+        input = input.reshape(ctx.batch * ctx.seq, ctx.in_dim)
         grad_output = grad_output.reshape(ctx.batch * ctx.seq, ctx.out_dim)
         
         # Re-randomize the rotation
@@ -55,7 +55,7 @@ class Nvidia_fn(torch.autograd.Function):
             
             grad_weight = F.linear(
                 grad_output.T,
-                input_fp4.T,
+                input.T,
                 None,
             )
             return grad_input, grad_weight, None, None
@@ -73,8 +73,8 @@ class Nvidia_fn(torch.autograd.Function):
         e_tht = (grad_output.T.reshape(-1, Nvidia_fn.hadamard_matrix.size(0)) @ Nvidia_fn.hadamard_matrix.T).reshape(ctx.out_dim, ctx.batch * ctx.seq)
         e_tht_fp4 = sr_1x16s_fp4_kernel_wrapper(e_tht, Nvidia_fn.hadamard_matrix.to(grad_output.dtype), (17 / 16), Nvidia_fn.group_size)
         
-        input_tht = (input_fp4.T.reshape(-1, Nvidia_fn.hadamard_matrix.size(0)) @ Nvidia_fn.hadamard_matrix.T).reshape(ctx.in_dim, ctx.batch * ctx.seq)
-        input_tht_fp4 = rtn_1x16s_fp4_kernel_wrapper(input_tht, Nvidia_fn.hadamard_matrix.to(input_fp4.dtype), 1.0, Nvidia_fn.group_size)
+        input_tht = (input.T.reshape(-1, Nvidia_fn.hadamard_matrix.size(0)) @ Nvidia_fn.hadamard_matrix.T).reshape(ctx.in_dim, ctx.batch * ctx.seq)
+        input_tht_fp4 = rtn_1x16s_fp4_kernel_wrapper(input_tht, Nvidia_fn.hadamard_matrix.to(input.dtype), 1.0, Nvidia_fn.group_size)
         
         grad_weight = F.linear(
             e_tht_fp4,
